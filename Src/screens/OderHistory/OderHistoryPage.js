bc1fbc121c238fcb9446e76aefa9a0d9e91e250f
abhishek rao <abhishek.rao@affle.com>
2021-05-27 11:55:24 +0530

    working on order history module

---
+++ Src/screens/OderHistory/OderHistoryPage.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+import { View,TouchableOpacity,Dimensions,Text,Image } from 'react-native';
+import assets from "../../Assets/index"
+class OderHistoryPage extends Component {
+    render() {
+        return (
+            <View
+                style={{ flex: 1 }}></View>
+        );
+    }
+}
+
+export default OderHistoryPage;
